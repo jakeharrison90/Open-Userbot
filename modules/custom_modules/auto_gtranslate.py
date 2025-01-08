8fe74a10a3965d47d1e2ca633cdf96577ac084cb
xzrci <itxmishii@gmail.com>
2025-01-08 08:27:08 +0500

    Update auto_gtranslate.py

--- modules/custom_modules/auto_gtranslate.py
+++ modules/custom_modules/auto_gtranslate.py
@@ -69,7 +69,6 @@ async def language_status(_, message: Message):
 @Client.on_message(filters.text & auto_translate_filter)
 async def auto_translate(_, message: Message):
     """Automatically translate and edit messages in chats with a set language."""
-    # Skip messages not sent by the user running the bot
     if message.from_user and not message.from_user.is_self:
         return
 
